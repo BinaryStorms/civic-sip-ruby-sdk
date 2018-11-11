22257f830f16ad1159db12de8dec9bd1d9c86d5d
Han Wang <han@thestorefront.com>
2018-11-10 17:12:30 -0800

    initial checkin

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'https://rubygems.org'
+
+ruby '~> 2.5.1'
+
+group :development, :test do
+  gem 'bundler', '~> 1.16'
+  gem 'rake', '~> 10.0'
+  gem 'rspec', '~> 3.0'
+end
---
+++ lib/binary_storms/civic_sip_sdk.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'binary_storms/civic_sip_sdk/version'
+
+module BinaryStorms
+  module CivicSIPSdk
+    # Your code goes here...
+  end
+end
