937ed518e11e30cba62d3ffe5c90c9898bd30fcf
Han Wang <han@thestorefront.com>
2018-11-10 22:03:46 -0800

    1. added RDoc for UserDataItem 2. added user_data with tests

---
+++ spec/binary_storms/user_data_spec.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+require 'binary_storms/civic_sip_sdk/user_data'
+require 'binary_storms/civic_sip_sdk/user_data_item'
+
+RSpec.describe BinaryStorms::CivicSIPSdk::UserData do
+  let(:raw_user_data_items) do
+    [
+      {
+        'label' => 'user.email',
+        'value' => 'big.boss@example.com',
+        'isValid' => true,
+        'isOwner' => true
+      },
+      {
+        'label' => 'user.firstName',
+        'value' => 'William',
+        'isValid' => false,
+        'isOwner' => true
+      }
+    ]
+  end
+
+  before(:each) do
+    @user_data = BinaryStorms::CivicSIPSdk::UserData.new(
+      user_id: 'user123',
+      data_items: raw_user_data_items
+    )
+  end
+
+  it 'should return a valid instance of UserData' do
+    expect(@user_data).to be_a(BinaryStorms::CivicSIPSdk::UserData)
+  end
+
+  it 'should return valid data from attr readers' do
+    expect(@user_data.user_id).to eq('user123')
+    expect(@user_data.data_items).to eq(raw_user_data_items)
+  end
+
+  it 'should return correct user data by label' do
+    user_data_item = @user_data.by_label(label: 'user.email')
+    expect(user_data_item).to be_a(BinaryStorms::CivicSIPSdk::UserDataItem)
+    expect(user_data_item.label).to eq('user.email')
+    expect(user_data_item.value).to eq('big.boss@example.com')
+    expect(user_data_item.is_valid).to be_truthy
+    expect(user_data_item.is_owner).to be_truthy
+  end
+
+  it 'should return nil if user data item label does not exist' do
+    expect(@user_data.by_label(label: 'not.here')).to be_nil
+  end
+end
